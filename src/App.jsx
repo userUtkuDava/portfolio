bb8da815ae0791d2067ecd02b302feb706344dbf
userUtkuDava <utkudava.js@gmail.com>
2024-09-26 18:17:26 +0300

    Section aralarında yavaş geçiş efekti eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,28 @@
 import './App.css'
 import Navbar from './Components/Navbar'
 import HomeHero from './Components/HomeHero'
+import About from './Components/About'
 function App() {
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
 
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth'
+      });
+    });
+  });
 
   return (
     <>
       <div className='index-content'>
         <Navbar />
         <HomeHero />
+        <About />
       </div>
     </>
   )
