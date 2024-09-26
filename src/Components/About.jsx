9a550fbc738d0276a94c4b59aad2523d646113c3
userUtkuDava <utkudava.js@gmail.com>
2024-09-26 18:44:31 +0300

    about page oluşturuldu

---
+++ src/Components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './style/css/about.css'
+
+const About = () => {
+    return (
+        <>
+            <section id="about" className='about'>
+                <div className='container'>
+                    <h2 className='heading-content'>
+                        <span className='title'>
+                            About Me
+                        </span>
+                    </h2>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default About
