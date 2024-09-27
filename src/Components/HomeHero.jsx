4907850935e8714c19ad861bae08e453207c4021
userUtkuDava <utkudava.js@gmail.com>
2024-09-27 06:19:48 +0300

    home page'deki sosyal medya bar'ı upgrade edildi

--- src/Components/HomeHero.jsx
+++ src/Components/HomeHero.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import './style/css/homeHero.css'
-import logoLinkedin from '../images/linkedin.png'
-import logoGithub from '../images/github.png'
+import logoWhatsapp from '../images/whatsappİconSB.png'
+import logoLinkedin from '../images/linkedinİconSB.png'
+import logoGithub from '../images/githubİconSB.png'
+
 
 
 const HomeHero = () => {
@@ -12,24 +14,39 @@ const HomeHero = () => {
                     Merhaba, ben Utku Dava</h1>
                 <div className='info'>
                     <p className='texting-primary'>
-                        Genel ürünün başarısına yol açan Web Sitelerini ve Web Uygulamalarını oluşturan ve yöneten Sonuç Odaklı bir Web Geliştiricisi
+                        Genel ürünün başarısına yol açan Web Sitelerini ve Web Uygulamalarını oluşturan ve yöneten
+                        <br />Sonuç Odaklı bir Web Geliştiricisi
+
                     </p>
                 </div>
                 <div className='home-hero__cta'>
-                    <a href="./#projects" class="btns btns--bg">Projects</a>
+                    <a href="./#projects" className="btns btns--bg">Projects</a>
 
                 </div>
             </div>
             <div className='home-hero__socials'>
-                <div class="home-hero__social">
+                <div className="home-hero__social">
                     <a href="https://github.com/userUtkuDava" class="home-hero__social-icon-link" rel="noreferrer" target="_blank">
-                        <img src={logoGithub} alt="Utku Dava Github Profil" class="home-hero__social-icon" />
+                        <img src={logoGithub} alt="Utku Dava Github Profil" className="home-hero__social-icon" />
                     </a>
                 </div>
-                <div class="home-hero__social">
+
+                <div className="home-hero__social">
                     <a href="https://www.linkedin.com/in/utku-dava-763548245/" className="home-hero__social-icon-link" rel="noreferrer" target="_blank">
                         <img src={logoLinkedin} alt="Utku Dava Linkedin Profili" className="home-hero__social-icon" />
                     </a>
+                </div>
+                <div class="home-hero__social">
+                    <a href="https://github.com/userUtkuDava" className="home-hero__social-icon-link" rel="noreferrer" target="_blank">
+                        <img src={logoWhatsapp} alt="Utku Dava Whatsapp" className="home-hero__social-icon" />
+
+                    </a>
+                </div>
+                <hr></hr>
+                <div class="home-hero__social last-info">
+                    <h2 className='text-bar'>Detaylı Bilgiler</h2>
+                    <i class="bi bi-yelp bar-icon"></i>
+
                 </div>
 
             </div>
