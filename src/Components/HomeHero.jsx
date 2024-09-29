c654f55409e0a327e537b179db964d1d8a4796fe
userUtkuDava <utkudava.js@gmail.com>
2024-09-29 07:39:33 +0300

    3 farklı görsel tema oluşturuldu

--- src/Components/HomeHero.jsx
+++ src/Components/HomeHero.jsx
@@ -1,63 +1,77 @@
-import React from 'react'
-import './style/css/homeHero.css'
-import logoWhatsapp from '../images/whatsappİconSB.png'
-import logoLinkedin from '../images/linkedinİconSB.png'
-import logoGithub from '../images/githubİconSB.png'
-
-
+import React, { useEffect } from 'react';
+import './style/css/homeHero.css';
+import logoWhatsapp from '../images/whatsappİconSB.png';
+import logoLinkedin from '../images/linkedinİconSB.png';
+import logoGithub from '../images/githubİconSB.png';
 
 const HomeHero = () => {
+    // Renk değiştirme fonksiyonu
+    const changeThemaRed = () => {
+        document.documentElement.style.setProperty('--primaryColor', '#ff0000'); // Yeni renk
+        document.documentElement.style.setProperty('--secondaryColor', '#4cf000');
+    };
+    const changeThemaOrange = () => {
+        document.documentElement.style.setProperty('--primaryColor', 'orange');
+        document.documentElement.style.setProperty('--secondaryColor', 'black'); // Yeni renk
+    };
+    const changeThemaBlue = () => {
+        document.documentElement.style.setProperty('--primaryColor', '#0071f3'); // Yeni renk
+        document.documentElement.style.setProperty('--secondaryColor', 'orange');
+    };
+
     return (
         <section className='home-hero'>
             <div className='home-hero__content'>
-                <h1 className='heading-primary'>
-                    Merhaba, ben Utku Dava</h1>
+                <h1 className='heading-primary'>Merhaba, ben Utku Dava</h1>
                 <div className='info'>
                     <p className='texting-primary'>
                         Genel ürünün başarısına yol açan Web Sitelerini ve Web Uygulamalarını oluşturan ve yöneten
                         <br />Sonuç Odaklı bir Web Geliştiricisi
-
                     </p>
                 </div>
                 <div className='home-hero__cta'>
                     <a href="./#projects" className="btns btns--bg">Projects</a>
-
                 </div>
             </div>
             <div className='home-hero__socials'>
                 <div className="home-hero__social">
-                    <a href="https://github.com/userUtkuDava" class="home-hero__social-icon-link" rel="noreferrer" target="_blank">
+                    <a href="https://github.com/userUtkuDava" className="home-hero__social-icon-link" rel="noreferrer" target="_blank">
                         <img src={logoGithub} alt="Utku Dava Github Profil" className="home-hero__social-icon" />
                     </a>
-                </div>
 
+                </div>
                 <div className="home-hero__social">
                     <a href="https://www.linkedin.com/in/utku-dava-763548245/" className="home-hero__social-icon-link" rel="noreferrer" target="_blank">
                         <img src={logoLinkedin} alt="Utku Dava Linkedin Profili" className="home-hero__social-icon" />
                     </a>
                 </div>
-                <div class="home-hero__social">
+                <div className="home-hero__social">
                     <a href="https://github.com/userUtkuDava" className="home-hero__social-icon-link" rel="noreferrer" target="_blank">
                         <img src={logoWhatsapp} alt="Utku Dava Whatsapp" className="home-hero__social-icon" />
-
                     </a>
                 </div>
-                <hr></hr>
-                <div class="home-hero__social last-info">
-                    <h2 className='text-bar'>Detaylı Bilgiler</h2>
-                    <i class="bi bi-yelp bar-icon"></i>
-
+                <div className='tema-container'>
+                    <div>
+                        <p className='tema-title'>Temaları Deneyin!</p>
+                        <div className='tema-buttons'>
+                            <div className='tema-button-one'>
+                                <button className='button-theme-blue ' id="changeColorBtn" onClick={changeThemaBlue}></button>
+                            </div>
+                            <div className='tema-button-one'>
+                                <button className='button-theme-red ' id="changeColorBtn" onClick={changeThemaRed}></button>
+                            </div>
+                            <div className='tema-button-one'>
+                                <button className='button-theme-orange' onClick={changeThemaOrange} id="changeColorBtn"></button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
             </div>
             <div className='home-hero__mouse-scroll-cont'>
-                <div className='mouse'>
-
-                </div>
+                <div className='mouse'></div>
             </div>
-
         </section>
-    )
-}
+    );
+};
 
-export default HomeHero
+export default HomeHero;
